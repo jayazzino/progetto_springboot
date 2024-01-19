aa4214e75c084f49ae6b4acca1d8ec431d03fa89
jayazzino <andreainternullo4@gmail.com>
2024-01-19 18:34:55 +0100

    implementazione jpa repo per importare da db

--- src/main/java/com/example/demo/Student/StudentService.java
+++ src/main/java/com/example/demo/Student/StudentService.java
@@ -4,17 +4,21 @@ import java.time.LocalDate;
 import java.time.Month;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class StudentService {
+
+    private final StudentRepository studentRepository;
+
+    @Autowired
+    public StudentService(StudentRepository studentRepository) {
+        this.studentRepository = studentRepository;
+    }
+
     public List<Student> getStudents() {
-        return List.of(
-                new Student(
-                        1L,
-                        "Andrea",
-                        "andrea@gmail.com",
-                        LocalDate.of(1998, Month.MARCH, 21),
-                        21));
+        return studentRepository.findAll();
+
     }
 }
